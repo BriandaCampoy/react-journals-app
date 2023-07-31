72bfd4bbc196a8bbf361753382d46d20205479c1
BriandaCampoy <brianda.campoy@gmail.com>
2023-07-31 02:01:14 -0600

    detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
+import { lazy, Suspense } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import Aside from './components/Aside';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Researcher from './pages/Researcher';
-import Journal from './pages/Journal';
-import ResearcherEdit from './pages/Researcher/Researcher-edit';
-import ResearcherList from './pages/Researchers-list';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
-import UploadJournal from './pages/Researcher/UploadJournal';
-import Profile from './pages/Researcher/Profile';
-import JournalView from './pages/Journal/JournalView';
-import JournalEdit from './pages/Journal/JournalEdit';
+// import Aside from './components/Aside';
+const Aside = lazy(() => import('./components/Aside'));
+const Nav = lazy(() => import('./components/Nav'));
+const Footer = lazy(() => import('./components/Footer'));
+const Home = lazy(() => import('./pages/Home'));
+const Researcher = lazy(() => import('./pages/Researcher'));
+const Journal = lazy(() => import('./pages/Journal'));
+const ResearcherEdit = lazy(() => import('./pages/Researcher/ResearcherEdit'));
+const ResearcherList = lazy(() => import('./pages/ResearchersList'));
+const Login = lazy(() => import('./pages/Login'));
+const SignUp = lazy(() => import('./pages/SignUp'));
+const UploadJournal = lazy(() => import('./pages/Researcher/UploadJournal'));
+const Profile = lazy(() => import('./pages/Researcher/Profile'));
+const JournalView = lazy(() => import('./pages/Journal/JournalView'));
+const JournalEdit = lazy(() => import('./pages/Journal/JournalEdit'));
+
 import { useUserContext } from './context/useUserContext';
 
 /**
@@ -38,28 +41,33 @@ function App() {
         <HashRouter>
           <Nav />
           <div id="layoutSidenav">
-            <Aside />
-            <div id="layoutSidenav_content">
-              <main>
-                <Routes>
-                  <Route path="/researcher" element={<Researcher />}>
-                    <Route path="upload-journal" element={<UploadJournal />} />
-                    <Route path="edit" element={<ResearcherEdit />} />
-                    <Route path="profile/:id" element={<Profile />} />
-                  </Route>
-                  <Route path="/journal" element={<Journal />}>
-                    <Route path="view/:id" element={<JournalView />} />
-                    <Route path="edit/:id" element={<JournalEdit />} />
-                  </Route>
-                  <Route
-                    path="/researchers-list"
-                    element={<ResearcherList />}
-                  />
-                  <Route path="/" element={<Home />} />
-                </Routes>
-              </main>
-              <Footer />
-            </div>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Aside />
+              <div id="layoutSidenav_content">
+                <main>
+                  <Routes>
+                    <Route path="/researcher" element={<Researcher />}>
+                      <Route
+                        path="upload-journal"
+                        element={<UploadJournal />}
+                      />
+                      <Route path="edit" element={<ResearcherEdit />} />
+                      <Route path="profile/:id" element={<Profile />} />
+                    </Route>
+                    <Route path="/journal" element={<Journal />}>
+                      <Route path="view/:id" element={<JournalView />} />
+                      <Route path="edit/:id" element={<JournalEdit />} />
+                    </Route>
+                    <Route
+                      path="/researchers-list"
+                      element={<ResearcherList />}
+                    />
+                    <Route path="/" element={<Home />} />
+                  </Routes>
+                </main>
+                <Footer />
+              </div>
+            </Suspense>
           </div>
         </HashRouter>
       </>
